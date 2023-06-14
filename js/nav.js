a058cc981ef4945c7fc8ed924964b1b1855f9676
DolenDeori <dulench.deori565@gmail.com>
2023-06-14 20:18:07 +0530

    completed navigation code and added portfolio page

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,17 @@
 "use strict";
+
 // Slecting DOM elements
 const hamberger_bar = document.querySelector(".hamberger .bar");
 const nav_link = document.querySelector(".nav_links");
+const overlay = document.querySelector(".overlay");
 
 hamberger_bar.addEventListener("click", function () {
   nav_link.classList.toggle("show");
+  overlay.classList.toggle("show");
+});
+
+overlay.addEventListener("click", () => {
+  nav_link.classList.remove("show");
+  overlay.classList.toggle("show");
+  hamberger_bar.checked = false;
 });
