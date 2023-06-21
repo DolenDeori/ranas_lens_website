ea8e22fd8fa67c2d22f855ea788411b3a4797883
DolenDeori <dulench.deori565@gmail.com>
2023-06-21 23:26:50 +0530

    updating portfolio gallery

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,27 +1,7 @@
-//? VARIABLE SELECTION
+"use strict";
+// Creating elemnts
+lightbox = document.createElement(div);
+imagebox = document.createElement("img");
 
-const filter_links = document.querySelector(".filter_links_container");
+//  
 
-const itembox = document.querySelectorAll(".itembox");
-filter_links.addEventListener("click", (e) => {
-  /* this code will add the active class to the selected 
-  link and remove active class from other links */
-
-  if (e.target.classList.contains("filter_links")) {
-    filter_links.querySelector(".active").classList.remove("active");
-    e.target.classList.add("active");
-
-    const filterValue = e.target.getAttribute("data-filter");
-    console.log(filterValue);
-
-    itembox.forEach((item) => {
-      if (item.classList.contains(filterValue) || filterValue === "all") {
-        item.classList.remove("hide_item");
-        item.classList.add("show_item");
-      } else {
-        item.classList.remove("show_item");
-        item.classList.add("hide_item");
-      }
-    });
-  }
-});
