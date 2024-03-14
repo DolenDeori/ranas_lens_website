170f566efff36d7447c7f6f3d6617956647900f4
DolenDeori <dulench.deori565@gmail.com>
2024-03-14 23:01:57 +0530

    navigation layout update

--- js/nav.js
+++ js/nav.js
@@ -4,14 +4,21 @@
 const hamberger_bar = document.querySelector(".hamberger .bar");
 const nav_link = document.querySelector(".nav_links");
 const overlay = document.querySelector(".overlay");
+const links = document.querySelectorAll(".links");
+const social_icons = document.querySelectorAll(".social_icons_2");
 
 hamberger_bar.addEventListener("click", function () {
   nav_link.classList.toggle("show");
-  overlay.classList.toggle("show");
+  links.forEach((link) => {
+    link.classList.toggle("fade");
+  });
+  social_icons.forEach((icons) => {
+    icons.classList.toggle("fade_icons");
+  });
 });
 
-overlay.addEventListener("click", () => {
-  nav_link.classList.remove("show");
-  overlay.classList.toggle("show");
-  hamberger_bar.checked = false;
-});
+// overlay.addEventListener("click", () => {
+//   nav_link.classList.remove("show");
+//   overlay.classList.toggle("show");
+//   hamberger_bar.checked = false;
+// });
