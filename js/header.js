e64bd16b9e1fb95b909483e3fb1907d8a8b0f3ce
DolenDeori <dulench.deori565@gmail.com>
2024-03-21 09:41:07 +0530

    portfolio section update

--- js/header.js
+++
@@ -1,8 +0,0 @@
-const image = document.querySelector(".hero_image");
-const header = document.querySelector(".hero_container");
-
-console.log(image.getAttribute("src"));
-
-header.addEventListener("click", () => {
-  image.setAttribute("src", `./assests/images/concert_1.webp`);
-});
