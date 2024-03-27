d3373fc24e81a6e46057760f35336cb208555c50
DolenDeori <dulench.deori564@gmail.com>
2024-03-27 21:59:10 +0530

    added animation to home page upaded some styles

---
+++ js/index.js
@@ -0,0 +1,20 @@
+const pageArea = document.querySelectorAll(".area");
+
+const animateArea = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section_hidden");
+  observer.unobserve(entry.target);
+};
+
+const areaObserve = new IntersectionObserver(animateArea, {
+  root: null,
+  threshold: 0.1,
+});
+
+pageArea.forEach((area) => {
+  areaObserve.observe(area);
+  area.classList.add("section_hidden");
+});
+
+// Insta card Animation
