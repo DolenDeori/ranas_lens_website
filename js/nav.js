8b2e4168aff3be8fd0b2d97e660e481faf2809dc
DolenDeori <dulench.deori564@gmail.com>
2024-03-28 00:06:22 +0530

    about section header v1 done

--- js/nav.js
+++ js/nav.js
@@ -2,13 +2,13 @@
 
 // Slecting DOM elements
 const hamberger_bar = document.querySelector(".hamberger .bar");
-const nav_link = document.querySelector(".nav_links");
+const nav_links = document.querySelector(".nav_links");
 const overlay = document.querySelector(".overlay");
 const links = document.querySelectorAll(".links");
 const social_icons = document.querySelectorAll(".social_icons_2");
 
 hamberger_bar.addEventListener("click", function () {
-  nav_link.classList.toggle("show");
+  nav_links.classList.toggle("show");
   links.forEach((link) => {
     link.classList.toggle("fade");
   });
